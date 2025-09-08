c20a6179ed0c69670b22c57d5d671c4ccbf52166
DandyCat91 <kate_poluektova@mail.ru>
2025-09-08 15:15:41 +0300

    Практикуемся в реализации API клиентов

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,22 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+class UsersRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+        Клиент для работы с /api/v1/users
+        """
+    def create_user_api(self, request: UsersRequestDict) -> Response:
+        return self.post("/api/v1/users", json= request)
